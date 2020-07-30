829488262551f7cb9522254c4150e3694f408652
9-Para <asmrahul17@gmail.com>
2020-07-30 21:47:16 +0530

    initial commit

---
+++ js/main.js
@@ -0,0 +1,46 @@
+/*  ---------------------------------------------------
+    Template Name: Violet 
+    Description: Violet ecommerce Html Template
+    Author: Colorlib
+    Author URI: https://colorlib.com/
+    Version: 1.0
+    Created: Colorlib
+---------------------------------------------------------  */
+
+'use strict';
+
+(function ($) {
+
+    /*------------------
+        Preloader
+    --------------------*/
+    $(window).on('load', function () {
+        $(".loader").fadeOut();
+        $("#preloder").delay(200).fadeOut("slow");
+    });
+
+  
+
+    /*------------------
+		Navigation
+	--------------------*/
+    $(".mobile-menu").slicknav({
+        appendTo: '.header-section',
+        allowParentLinks: true,
+        closedSymbol: '<i class="fa fa-angle-right"></i>',
+		openedSymbol: '<i class="fa fa-angle-down"></i>'
+    });
+
+
+    /*------------------
+        Magnific Popup
+    --------------------*/
+    $('.pop-up').magnificPopup({
+        type: 'image'
+    });
+
+
+    
+   
+
+})(jQuery);
